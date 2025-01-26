1c60b2a736c6f42dd7334871c5ebb4f2219a5d6a
simon <simon.graeber@gmx.net>
2025-01-26 01:10:48 +0100

    FE set up

---
+++ frontend/src/test/java/com/fragen/frontend/AppTest.java
@@ -0,0 +1,12 @@
+package com.fragen.frontend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void basicTest() {
+        assertTrue(true);
+    }
+}
