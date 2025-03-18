38ec13bb5cca664fb9cbe2ebda4a2028adfb2fa4
simon <simon.graeber@gmx.net>
2025-03-18 13:35:12 +0100

    update imprint pages with animations

--- frontend/src/main/frontend/src/components/custom/legal/ImprintPage.tsx
+++ frontend/src/main/frontend/src/components/custom/legal/ImprintPage.tsx
@@ -30,40 +30,43 @@ function ImprintPage() {
             boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
           }}
           transition={{ duration: 0.3 }}
-          className="relative mx-auto max-w-2xl p-6 rounded-lg shadow-lg bg-gradient-to-br from-gray-100 to-gray-50 dark:from-gray-800 dark:to-gray-900"
+          className="relative mx-auto max-w-md p-6 md:py-12 rounded-lg shadow-xl bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-800 dark:to-gray-900"
         >
           {/* Subtle Gradient Background */}
           <div
-            className="absolute top-0 left-0 w-64 h-64 bg-gradient-to-br from-purple-300 to-indigo-400
-                       dark:from-purple-700 dark:to-indigo-900 rounded-full filter blur-3xl opacity-20 animate-pulse-slow"
+            className="absolute top-0 left-0 w-40 h-40 bg-gradient-to-br from-purple-300 to-indigo-400
+                       dark:from-purple-700 dark:to-indigo-900 rounded-full filter blur-2xl opacity-20 animate-pulse-slow"
           ></div>
 
           {/* Layout */}
           <div className="relative z-10 flex flex-col md:flex-row items-center gap-6">
-            {/* Left Column - Centered Logo */}
-            <div className="flex-shrink-0 flex flex-col items-center justify-center w-3/5">
-              {/* Larger square logo */}
+            {/* Left Column - Logo & Animation */}
+            <div className="flex-shrink-0 flex flex-col items-center justify-center md:w-2/5">
               <img
                 src="/icon.svg"
                 alt="Logo"
-                className="w-32 h-32 mb-4 rounded-lg object-cover"
+                className="w-24 h-24 mb-2 rounded-lg object-cover"
               />
-              <GreetingsAnimation />
+              <div className="relative overflow-visible">
+                <div className="pl-2 ptransform scale-[1.35] rotate-[-6deg]">
+                  <GreetingsAnimation />
+                </div>
+              </div>
             </div>
 
             {/* Right Column - Name and Contact Information */}
-            <div className="flex flex-col space-y-4 w-2/5 pr-3">
-              {/* Name aligned to the left */}
+            <div className="flex flex-col space-y-4 md:w-3/5 pr-2">
+              {/* Name */}
               <div className="flex items-center">
-                <FaUser className="text-primary mr-2 text-3xl" />
-                <h2 className="font-bold text-2xl">Simon Graeber</h2>
+                <FaUser className="text-primary mr-2 text-2xl" />
+                <h2 className="font-bold text-xl">Simon Graeber</h2>
               </div>
-              <div className="h-1 w-20 bg-primary mb-4"></div>
+              <div className="h-0.5 w-16 bg-primary mb-2"></div>
 
               {/* Contact Details */}
-              <div className="flex flex-col space-y-4">
+              <div className="flex flex-col space-y-2 text-sm">
                 <div className="flex items-start">
-                  <FaMapMarkerAlt className="text-gray-600 mr-2 mt-1 min-w-[16px]" />
+                  <FaMapMarkerAlt className="text-secondary-foreground mr-2 mt-1" />
                   <div>
                     <p>Mitthenheimer Str. 6</p>
                     <p>85764 Oberschleißheim</p>
@@ -71,8 +74,16 @@ function ImprintPage() {
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <FaEnvelope className="text-gray-600 mr-2 min-w-[16px]" />
-                  <p>80-read-crewel@icloud.com</p>
+                  <div>
+                    <FaEnvelope className="text-secondary-foreground -mr-2 mt-1" />
+                  </div>
+                  <div>
+                    <Button variant="link" className="mt-1" asChild>
+                      <a href="mailto:80-read-crewel@icloud.com">
+                        80-read-crewel@icloud.com
+                      </a>
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
